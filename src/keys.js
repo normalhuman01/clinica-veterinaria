462cc5ffa185d778620ef834710484c559773ab4
Composta Puerta <adkadkwkq12@gmail.com>
2024-01-05 16:11:11 -0500

    upload
    update

---
+++ src/keys.js
@@ -0,0 +1,22 @@
+//la configuración de la base de datos
+
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'FisiCalidad',
+        database: 'petswork3'
+            /*
+            host: 'us-cdbr-east-03.cleardb.com',
+            user: 'b80ae93b29bbd1',
+            password: '1b2341ed',
+            database: 'heroku_1f9e9f148cd438f'
+            */
+            /*
+            host: 'us-cdbr-east-03.cleardb.com',
+            user: 'b8e34158328c55',
+            password: 'c4b0ecb4',
+            database: 'heroku_c9f3c988abe32e2'
+            */
+    }
+};
